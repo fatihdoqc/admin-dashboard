5b056748ac9f1f01028ecc9616e45ef623c2976d
fatih.doqc <fatih.doqc@gmail.com>
2023-09-03 07:27:50 +0300

    feature: add database connection and test writing to database

--- views/home.php
+++ views/home.php
@@ -1,2 +1,26 @@
 <?php
-    echo "I am vengeance!";
+
+    $host = getenv('MYSQL_HOST');
+    $dbName = getenv('MYSQL_DB');
+    $user = getenv('MYSQL_USER');
+    $pass = getenv('MYSQL_PASSWORD');
+
+    try{
+        $connection = new PDO($host . '; dbname='. $dbName, $user, $pass);
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        echo "DB CASLti! ";
+
+        $userQuery = "INSERT INTO denemeTable (denemeCol) VALUES (:str)";
+
+        $stmt = $connection->prepare($userQuery);
+
+        $stmt->execute(['str' => 'BOOM']);
+
+        echo " Yazdim.";
+
+    }catch (PDOException $exception){
+        echo "Database error" . $exception;
+    }
+
+?>
