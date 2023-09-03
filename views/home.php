2b5c0b29b1fb3b512443c12dabd227e0cf18f114
fatih.doqc <fatih.doqc@gmail.com>
2023-09-04 01:55:20 +0300

    feature: add local database connection and test writing to local database

--- views/home.php
+++ views/home.php
@@ -1,19 +1,25 @@
 <?php
 
-    $host = getenv('MYSQL_HOST');
-    $dbName = getenv('MYSQL_DB');
-    $user = getenv('MYSQL_USER');
-    $pass = getenv('MYSQL_PASSWORD');
+    //$host = getenv('MYSQL_HOST');
+    $host = "127.0.0.1";
+    //$dbName = getenv('MYSQL_DB');
+    $dbName = "dashboardlocal";
+//    $user = getenv('MYSQL_USER');
+    $user = "root";
+//    $pass = getenv('MYSQL_PASSWORD');
+    $pass = "";
 
     try{
-        $connection = new PDO($host . '; dbname='. $dbName, $user, $pass);
-        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo $user;
+        $dsn = "mysql:host=" . $host . ";dbname=" . $dbName;
+        $conn = new PDO($dsn, $user, $pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         echo "DB CASLti! ";
 
-        $userQuery = "INSERT INTO denemeTable (denemeCol) VALUES (:str)";
+        $userQuery = "INSERT INTO users (email) VALUES (:str)";
 
-        $stmt = $connection->prepare($userQuery);
+        $stmt = $conn->prepare($userQuery);
 
         $stmt->execute(['str' => 'BOOM']);
 
